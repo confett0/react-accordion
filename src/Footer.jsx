c611986d65913ceae077f91ceec8c2c604c266c2
confett0 <118047961+confett0@users.noreply.github.com>
2024-03-15 15:23:20 +0100

    Add footer

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer>
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/confett0">Confett0</a>.
+      </footer>
+    )
+}
